53e642ea29d930333dda9519ac45845f36b509af
emailanuj <anujdubey@Anujs-MacBook-Air.local>
2018-03-24 17:42:22 +0530

    Twig Usage Ends

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -26,7 +26,12 @@ class ArticleController extends AbstractController
      * @Route("/news/{slug}")
      */
     public function show($slug){
-     return $this->render('article/show.html.twig',['title'=>ucwords(str_replace('-',' ',$slug))]);
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+     return $this->render('article/show.html.twig',['comments'=>$comments,]);
     }
 
 }
